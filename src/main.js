f468d105a106555414f0b7ec91c8167ec8023492
Sanjula <sanjula.madumal@gmail.com>
2021-08-03 19:58:51 +0530

    add sample dom and storybook

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue,CardPlugin  } from 'bootstrap-vue'
 
+
+Vue.use(BootstrapVue)
+Vue.use(CardPlugin )
 Vue.config.productionTip = false;
 
 new Vue({
